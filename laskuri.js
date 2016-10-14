9a3af99f410234d8d164558c9ba61b609a2e26ee
Katja Kortelainen <kkortela@Katja-MacBook-Air.local>
2016-10-14 23:19:48 +0300

    First commit

---
+++ laskuri.js
@@ -0,0 +1,36 @@
+var deadline = 'July 08 2017 14:00:00 GMT+0200';
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime){
+  var clock = document.getElementById(id);
+  var timeinterval = setInterval(function(){
+    var t = getTimeRemaining(endtime);
+    clock.innerHTML = '<span class="number">' + t.days + '</span>' +
+                      '<span class="time">' + ' pvä' + '</span>' +
+                      '<span class="number">' + t.hours + '</span>' +
+                      '<span class="time">' + ' t' + '</span>' +
+                      '<span class="number">' + t.minutes + '</span>' +
+                      '<span class="time">' + ' min' + '</span>' +
+                      '<span class="number">' + t.seconds + '</span>' +
+                      '<span class="time">' + ' s' + '</span>'
+    if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+  },1000);
+}
+
+initializeClock('counter', deadline);
